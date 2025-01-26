34815d9cb6c07b129e4ca1ffeacfeea5ddb9bc1b
MalovAleksey <128486566+MalovAleksey@users.noreply.github.com>
2025-01-26 16:57:14 +0300

    Create ping.sh

---
+++ ping.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PREFIX=$1
+INTERFACE=$2
+
+trap 'echo "Ping exit (Ctrl-C)"; exit 1' 2
+
+for SUBNET in {0..255}
+do
+
+for HOST in {1..255}
+do
+echo "IP:$PREFIX.$SUBNET.$HOST"
+arping -c 3 -I $INTERFACE $PREFIX"."$SUBNET"."$HOST 2> /dev/null
+
+done
+done
